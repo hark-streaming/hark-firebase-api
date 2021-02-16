cca59ab2a47e1cf0961a2849daa9678d27789fb1
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-02-15 21:31:30 -0800

    dummy register endpoint

--- functions/src/api/users/index.ts
+++ functions/src/api/users/index.ts
@@ -1,5 +1,8 @@
 // Based from https://medium.com/@atbe/firebase-functions-true-routing-2cb17a5cd288
 import * as express from "express";
+import * as admin from "firebase-admin";
+
+
 
 // This is the router which will be imported in our
 // api hub (the index.ts which will be sent to Firebase Functions).
@@ -16,6 +19,21 @@ userRouter.get("/:uid", async function getUser(req: express.Request, res: expres
 
   // ...
 });
+
+userRouter.post("/register", async function getUser(req: express.Request, res: express.Response) {
+    // the firestore
+    const db = admin.firestore();
+       
+    await db.collection("users").doc("testregister").set({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            captcha: req.body.captchaToken,
+    });
+
+    res.status(200).send("User registered!");
+});
+
 
 // Useful: Let's make sure we intercept un-matched routes and notify the client with a 404 status code
 userRouter.get("*", async (req: express.Request, res: express.Response) => {
