fd4b05d53e06472d8383d6875fbf77126a3e3df6
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-02-15 21:31:54 -0800

    add user to firestore when user created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,9 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import * as usersApi from "./api/users";
 
-admin.initializeApp(functions.config().firebase);
-
-//const db = admin.firestore();
+//admin.initializeApp(functions.config().firebase);
+// reminder: https://stackoverflow.com/questions/57397608/the-default-firebase-app-does-not-exist-make-sure-you-call-initializeapp-befo
+admin.initializeApp();
 
 const app = express();
 // https://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
@@ -17,8 +17,24 @@ app.use("/users", usersApi.userRouter);
 // Again, lets be nice and help the poor wandering servers, any requests to /api
 // that are not /api/users will result in 404.
 app.get("*", async (req: express.Request, res: express.Response) => {
-	res.status(404).send("This route does not exist.");
+    res.status(404).send("This route does not exist.");
 });
 
 exports.api = functions.https.onRequest(app);
+
+
+// additional listeners
+
+// adds user to database upon creation
+exports.addUserToDb = functions.auth.user().onCreate(async (user) => {
+    // the firestore
+    const db = admin.firestore();
+    await db.collection("users").doc(user.uid).set({
+        username: user.displayName,
+        email: user.email,
+        uid: user.uid,
+        /*password: this.user.password,
+        captcha: this.captchaToken,*/
+    });
+});
 
